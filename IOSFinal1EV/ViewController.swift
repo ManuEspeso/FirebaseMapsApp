f2bada4a0ee8dff62fa08929cf1ca4b1e3bbc0f3
Manu <espesito16@gmail.com>
2019-11-30 08:43:22 -0800

    Firebase Implementation Now the proyect constains Firebase and it works propertly

--- IOSFinal1EV/ViewController.swift
+++ IOSFinal1EV/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginUser()
     }
 
-//
+    func loginUser() {
+        
+        Auth.auth().signIn(withEmail: "manu@gmail.com", password: "password") { (user, error) in
+            let user = Auth.auth().currentUser
+            if let user = user {
+                
+                print("-------------->", user.uid)
+            } else {
+                
+                print(error!)
+            }
+        }
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IOSFinal1EV' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IOSFinal1EV
+	pod 'Firebase'
+  	pod 'FirebaseAuth'
+  	pod 'FirebaseFirestore'
+  	pod 'FirebaseCore'
+  	pod 'GoogleSignIn'
+
+end
