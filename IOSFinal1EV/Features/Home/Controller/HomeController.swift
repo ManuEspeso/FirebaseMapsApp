92c843f91a65f956176fa7efbe89359af30a9e68
Manu <espesito16@gmail.com>
2019-11-30 17:23:21 -0800

    Work in navigation between views I'm implement a method who auto rediriged to Home page. In the future it will be contains for the autologin. Also in the home page I'm create a button who return in to login page, in futures updates also do a remove user from core data when its clicked.

--- IOSFinal1EV/Features/Home/Controller/HomeController.swift
+++ IOSFinal1EV/Features/Home/Controller/HomeController.swift
@@ -10,14 +10,18 @@ import UIKit
 import Firebase
 import FirebaseAuth
 
-class ViewController: UIViewController {
-
+class HomeController: UIViewController {
+    
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loginUser()
     }
-
+    
     func loginUser() {
         
         Auth.auth().signIn(withEmail: "manu@gmail.com", password: "password") { (user, error) in
@@ -32,4 +36,16 @@ class ViewController: UIViewController {
         }
     }
 }
+
+/*extension UITextField {
+    func textField(withPlaceolder placeholder: String, isSecureTextEntry: Bool) -> UITextField {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.isSecureTextEntry = isSecureTextEntry
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        return tf
+    }
+}*/
 
---
+++ IOSFinal1EV/Features/Login/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  IOSFinal1EV
+//
+//  Created by Manu Espeso on 30/11/2019.
+//  Copyright © 2019 Manu Espeso. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.performSegue(withIdentifier: "showHomePage", sender: self)
+        })*/
+    }
+}
